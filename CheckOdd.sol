b823b61cac92f6e242c5f23ed7f80602ff08c648
LeBarney <barneywarframe@gmail.com>
2021-05-11 16:34:39 +0200

    3eme contrat

---
+++ CheckOdd.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract CheckOdd {
+    function check(uint256 value) public pure returns (bool){
+        if (value % 2 != 0 ){
+            return true;
+        } else {
+            return false;
+            }
+                                                            }
+                    }
